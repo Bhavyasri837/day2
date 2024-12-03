cea9d5734461267a61a12ae06477eb1b10a84945
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-03 15:58:53 +0530

    Add files via upload

---
+++ shiftoperations.java
@@ -0,0 +1,12 @@
+public class shiftoperations {
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=1;
+        System.out.println(a<<2);
+        System.out.println(b>>5); 
+        System.out.println(a>>6);
+        System.out.println(b<<4);
+    }
+    
+}
