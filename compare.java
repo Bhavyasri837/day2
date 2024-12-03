d91ee22d0407c42f9684458d585c69e3449a4a06
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-03 10:17:49 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,14 @@
+public class compare {
+    public static void main(String args[])
+    {
+        int a=12;
+        float b=12.01f;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+    
+}
