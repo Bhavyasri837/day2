1ad68810a3dd32d3b74c23cb8ad7039d02344a6e
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-03 10:11:23 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,15 @@
+class demo
+{
+    public static void main(String args[])
+    {
+       int a=10;
+       int b=12;
+       int c=2;
+       int d=5;
+       System.out.println("addition="+(a+b));
+       System.out.println("substraction="+(a-c));
+       System.out.println("multiplication="+(b*d));
+       System.out.println("division="+(b/c));
+       System.out.println("modulo division="+(b%d));
+    }
+}
